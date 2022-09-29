11089bfab9337d072bdadb7c3047c276ea3af73b
renanfelipelopes <renanlopes.ads@gmail.com>
2022-09-29 20:36:43 -0300

    MinimalApi Project

---
+++ MinimalAPI/Data/MinimalContextDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Models;
+
+namespace MinimalAPI.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("Fornecedor");
+                
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnetion")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
