4832a4f9f75defe73814b9b30ac7ed67290dff62
renanfelipelopes <renanlopes.ads@gmail.com>
2022-10-12 18:31:21 -0300

    Adicionando MinimalValidation

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Data;
 using MinimalAPI.Models;
+using MiniValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,9 +39,17 @@ app.MapGet("/fornecedor/{id}", async (Guid id, MinimalContextDb context) =>
 
 app.MapPost("/fornecedor", async (MinimalContextDb context, Fornecedor fornecedor) =>
 {
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
     context.Fornecedores.Add(fornecedor);
     var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor)
+        : Results.BadRequest("Houve um problema ao salvar o registro!");
 })
+.ProducesValidationProblem()
 .Produces<Fornecedor>(StatusCodes.Status201Created)
 .Produces(StatusCodes.Status400BadRequest)
 .WithName("PostFornecedor")
