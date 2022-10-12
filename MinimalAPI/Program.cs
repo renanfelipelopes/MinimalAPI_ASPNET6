c9c515696cf91f0ad652ba2ff6c9417083fe5be0
renanfelipelopes <renanlopes.ads@gmail.com>
2022-10-12 17:46:48 -0300

    Criando endpoint de obter por id

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Data;
+using MinimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,9 +22,21 @@ app.UseHttpsRedirection();
 
 app.MapGet("/fornecedor", async(
     MinimalContextDb context) => 
-    await context.Fornecedores.ToListAsync()
-)
-.WithName("GetFornecedor")
-.WithTags("Fornecedor");
+    await context.Fornecedores.ToListAsync())
+    .WithName("GetFornecedor")
+    .WithTags("Fornecedor");
+
+app.MapGet("/fornecedor/{id}", async (
+    Guid id, 
+    MinimalContextDb context) =>
+    
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor
+            ? Results.Ok()
+            : Results.NotFound())
+    .Produces<Fornecedor>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetFornecedorPorId")
+    .WithTags("Fornecedor");
 
 app.Run();
